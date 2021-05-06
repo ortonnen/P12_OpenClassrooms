5d52a61ba4f2535837e1cb53b4c133b247a26ad3
ortonnen <57671772+ortonnen@users.noreply.github.com>
2021-05-06 22:34:59 +0200

    add API Key

---
+++ WaWe/Other file/ApiKey.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKey.swift
+//  Wawe
+//
+//  Created by Nathalie Simonnet on 05/01/2021.
+//
+
+import Foundation
+
+class ApiKey {
+    static var translateApiKey = "AIzaSyDCNJ6S52AdcHjE9lL7sHC9T9aYxXs5nhE"
+    static var spoonacularApiKey = "cda7fd4246c3460b8e5d752e105cbc8c"
+}
