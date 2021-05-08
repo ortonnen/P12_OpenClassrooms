4314e36acb26fc599997b086455f0a959ccd11ee
ortonnen <57671772+ortonnen@users.noreply.github.com>
2021-05-08 17:06:05 +0200

    add More Filtre to Search Recipe

---
+++ WaWe/Controller/Search Recipe/MoreInformationViewController.swift
@@ -0,0 +1,213 @@
+//
+//  MoreInformationViewController.swift
+//  Wawe
+//
+//  Created by Nathalie Simonnet on 15/03/2021.
+//
+
+import UIKit
+//MARK: - More Information View Controller
+class MoreInformationViewController: UIViewController {
+    
+    //MARK: Properties
+    var tableViewArray = SpoonacularParameters()
+    var cuisines = [String]()
+    var intolerances = [String]()
+    var diets = [String]()
+    
+    var typeOfCuisineSelected = [String]()
+    var typeOfDietSelected = [String]()
+    var typeOfIntolerancesSelected = [String]()
+    var typeOfMealSelected = [String]()
+    
+//    weak var delegate: PassingDataDelegateProtocol?
+    
+    //MARK: Outlet
+    @IBOutlet weak var typeOfCuisinesTableView: UITableView!
+    @IBOutlet weak var intolerancesTableView: UITableView!
+    @IBOutlet weak var typeOfDietTableView: UITableView!
+    @IBOutlet weak var typeOfMealSegmentedControl: UISegmentedControl!
+    
+    //MARK: Methods Override
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        typeOfDietTableView.delegate = self
+        typeOfDietTableView.dataSource = self
+        
+        intolerancesTableView.delegate = self
+        intolerancesTableView.dataSource = self
+        
+        typeOfCuisinesTableView.delegate = self
+        typeOfCuisinesTableView.dataSource = self
+        
+        translateCuisine()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        typeOfDietTableView.reloadData()
+        typeOfCuisinesTableView.reloadData()
+        intolerancesTableView.reloadData()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+//        delegate?.updateData(for: typeOfDietSelected, intoleranceArray: typeOfIntolerancesSelected, cuisineArray: typeOfCuisineSelected, mealArray: typeOfMealSelected)
+    }
+    
+    //MARK: Actions
+    @IBAction func mealTypeSegmentedControl(_ sender: Any) {
+        if typeOfMealSegmentedControl.selectedSegmentIndex == 1 {
+            typeOfMealSelected = tableViewArray.mealTypesEntree
+        }
+        if typeOfMealSegmentedControl.selectedSegmentIndex == 2 {
+            typeOfMealSelected = tableViewArray.mealTypesMainDish
+        }
+        if typeOfMealSegmentedControl.selectedSegmentIndex == 3 {
+            typeOfMealSelected = tableViewArray.mealTypesDessert
+        }
+    }
+    
+    //MARK: File Private Func
+    ///method to translate element in cuisine array
+    fileprivate func translateCuisine() {
+        translate(from: "en", to: "fr", for: tableViewArray.cuisines.joined(separator: ",")) { (translatedText) in
+            self.cuisines = translatedText.components(separatedBy: ",")
+            self.typeOfCuisinesTableView.reloadData()
+            self.translateIntolerance()
+        }
+    }
+    ///method to translate element in diet array
+    fileprivate func translateDiet() {
+        translate(from: "en", to: "fr", for: tableViewArray.diets.joined(separator: ",")) { (translatedText) in
+            self.diets = translatedText.components(separatedBy: ",")
+            self.typeOfDietTableView.reloadData()
+        }
+    }
+    ///method to translate element in intolerances array
+    fileprivate func translateIntolerance() {
+       translate(from: "en", to: "fr", for: tableViewArray.intolerances.joined(separator: ",")) { (translatedText) in
+            self.intolerances = translatedText.components(separatedBy: ",")
+            self.intolerancesTableView.reloadData()
+            self.translateDiet()
+        }
+    }
+    ///method to translate String
+    fileprivate func translate(from detectedLang: String, to desiredLang: String, for text: String, completion: @escaping(String) -> Void) {
+        TranslationService.shared.getTranslate(from: detectedLang, to: desiredLang, for: text) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    completion(response.data.translations[0].translatedText)
+                case .failure(let error):
+                    print(error)
+                    break
+                }
+            }
+        }
+    }
+}
+
+//MARK: - TableView
+extension MoreInformationViewController: UITableViewDataSource, UITableViewDelegate {
+    ///method to determine number of Row in Section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var count: Int?
+        if tableView == typeOfCuisinesTableView {
+            count = tableViewArray.cuisines.count
+        }
+        if tableView == intolerancesTableView {
+            count = tableViewArray.intolerances.count
+        }
+        if tableView == typeOfDietTableView {
+            count = tableViewArray.diets.count
+        }
+        return count ?? 4
+    }
+    ///method to determine cell for Row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if tableView == typeOfCuisinesTableView{
+            var cell = tableView.dequeueReusableCell(withIdentifier: "typeOfCuisineCell", for: indexPath) as? FilterTableViewCell
+            configureCell(&cell, tableView, indexPath, for: .cuisine, and: cuisines)
+            return cell ?? UITableViewCell()
+        }
+        if tableView == intolerancesTableView{
+            var cell = tableView.dequeueReusableCell(withIdentifier: "intoleranceCell", for: indexPath) as? FilterTableViewCell
+            configureCell(&cell, tableView, indexPath, for: .intolerance, and: intolerances)
+            return cell ?? UITableViewCell()
+        }
+        if tableView == typeOfDietTableView{
+            var cell = tableView.dequeueReusableCell(withIdentifier: "typeOfDietCell", for: indexPath) as? FilterTableViewCell
+            configureCell(&cell, tableView, indexPath, for: .diet, and: diets)
+            return cell ?? UITableViewCell()
+        }
+      return UITableViewCell()
+    }
+    /// method to determine the behavior of a cell when she did select
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if tableView == typeOfCuisinesTableView {
+            cell?.setSelected(true, animated: true)
+            typeOfCuisineSelected.append(tableViewArray.cuisines[indexPath.row])
+        }
+        if tableView == intolerancesTableView{
+            cell?.setSelected(true, animated: true)
+            typeOfIntolerancesSelected.append(tableViewArray.intolerances[indexPath.row])
+        }
+        if tableView == typeOfDietTableView{
+            cell?.setSelected(true, animated: true)
+            typeOfDietSelected.append(tableViewArray.diets[indexPath.row])
+        }
+    }
+    /// method to determine the behavior of a cell when she didDeselect
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if tableView == typeOfCuisinesTableView {
+            cell?.setSelected(false, animated: true)
+            guard let index = typeOfCuisineSelected.firstIndex(of: tableViewArray.cuisines[indexPath.row]) else { return }
+            typeOfCuisineSelected.remove(at: index)
+        }
+        if tableView == intolerancesTableView{
+            cell?.setSelected(false, animated: true)
+            guard let index = typeOfIntolerancesSelected.firstIndex(of: tableViewArray.intolerances[indexPath.row]) else { return }
+            typeOfIntolerancesSelected.remove(at: index)
+        }
+        if tableView == typeOfDietTableView{
+            cell?.setSelected(false, animated: true)
+            guard let index = typeOfDietSelected.firstIndex(of: tableViewArray.diets[indexPath.row]) else { return }
+            typeOfDietSelected.remove(at: index)
+        }
+    }
+    
+    //MARK: File private method
+    ///method to configure cell
+    fileprivate func configureCell(_ cell: inout FilterTableViewCell?, _ tableView: UITableView, _ indexPath: IndexPath, for filter: Filter, and array: [String] ) {
+        cell?.titleIsHidden(for: filter )
+        if array.count > 0 {
+            let title = array[indexPath.row]
+            cell?.configure(withTitle: title, for: filter)
+        }
+    }
+    ///method to remove unselected element in array
+    fileprivate func removeArray(_ array: [String], _ selection: String) {
+        if let index = array.firstIndex(of: selection) {
+            if array == typeOfCuisineSelected {
+                typeOfCuisineSelected.remove(at: index)
+            } else if array == typeOfDietSelected {
+                typeOfDietSelected.remove(at: index)
+            } else if array == typeOfIntolerancesSelected {
+                typeOfIntolerancesSelected.remove(at: index)
+            }
+        }
+    }
+    ///method to append selected element in array
+    fileprivate func appendToArray(_ array: [String], _ selection: String) {
+        if array == typeOfCuisineSelected {
+            typeOfCuisineSelected.append(selection)
+        } else if array == typeOfDietSelected {
+            typeOfDietSelected.append(selection)
+        } else if array == typeOfIntolerancesSelected {
+            typeOfIntolerancesSelected.append(selection)
+        }
+    }
+}
---
+++ WaWe/View/TableViewCell/FilterTableViewCell.swift
@@ -0,0 +1,98 @@
+//
+//  FilterTableViewCell.swift
+//  Wawe
+//
+//  Created by Nathalie Simonnet on 26/03/2021.
+//
+
+import UIKit
+//MARK: Enum Filter
+enum Filter {
+    case cuisine, intolerance, diet
+}
+
+//MARK: Filter Table View Cell
+class FilterTableViewCell: UITableViewCell {
+
+    //MARK: Outlet
+    @IBOutlet weak var cuisineTitle: UILabel!
+    @IBOutlet weak var intoleranceTitle: UILabel!
+    @IBOutlet weak var dietTitle: UILabel!
+    
+    @IBOutlet weak var cuisineActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var intoleranceActivityIndiactor: UIActivityIndicatorView!
+    @IBOutlet weak var dietActivityIndiactor: UIActivityIndicatorView!
+   
+    //MARK: Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            self.textLabel?.textColor = #colorLiteral(red: 0.9633114934, green: 0.9485848546, blue: 0.8967990279, alpha: 1)
+            self.backgroundColor = #colorLiteral(red: 0.643104732, green: 0.2448115349, blue: 0.1737442911, alpha: 1)
+        } else {
+            self.textLabel?.textColor = #colorLiteral(red: 0.643104732, green: 0.2448115349, blue: 0.1737442911, alpha: 1)
+            self.backgroundColor = #colorLiteral(red: 0.9633114934, green: 0.9485848546, blue: 0.8967990279, alpha: 1)
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.textLabel?.textColor = #colorLiteral(red: 0.643104732, green: 0.2448115349, blue: 0.1737442911, alpha: 1)
+        self.backgroundColor = #colorLiteral(red: 0.9633114934, green: 0.9485848546, blue: 0.8967990279, alpha: 1)
+    }
+    
+    ///configure cell appearance
+    func configure(withTitle title: String, for cell : Filter){
+        if cell == .cuisine {
+            activityIndiactorIsHidden(for: .cuisine, with: title)
+        }
+        if cell == .intolerance {
+            activityIndiactorIsHidden(for: .intolerance, with: title)
+        }
+        if cell == .diet {
+            activityIndiactorIsHidden(for: .diet, with: title)
+        }
+    }
+    
+   func titleIsHidden(for tableCell: Filter) {
+        switch tableCell {
+        case .cuisine:
+            cuisineActivityIndicator.startAnimating()
+            cuisineActivityIndicator.isHidden = false
+            cuisineTitle.isHidden = true
+        case .intolerance:
+            intoleranceActivityIndiactor.startAnimating()
+            intoleranceActivityIndiactor.isHidden = false
+            intoleranceTitle.isHidden = true
+        case .diet:
+            dietActivityIndiactor.startAnimating()
+            dietActivityIndiactor.isHidden = false
+           dietTitle.isHidden = true
+        }
+    }
+    
+    //MARK: File Private Methods
+    fileprivate func activityIndiactorIsHidden(for tableCell: Filter, with title: String){
+        switch tableCell {
+        case .cuisine:
+            cuisineTitle.text = title
+            cuisineActivityIndicator.isHidden = true
+            cuisineActivityIndicator.stopAnimating()
+            cuisineTitle.isHidden = false
+        case .intolerance:
+            intoleranceTitle.text = title
+            intoleranceActivityIndiactor.isHidden = true
+            intoleranceActivityIndiactor.stopAnimating()
+            intoleranceTitle.isHidden = false
+        case .diet:
+            dietTitle.text = title
+            dietActivityIndiactor.isHidden = true
+            dietActivityIndiactor.stopAnimating()
+            dietTitle.isHidden = false
+        }
+    }
+}
